4301a9082b7648befb263cb9bc70efff8322cfe7
silasb <silas.baronda@gmail.com>
2014-04-22 23:42:34 -0400

    Getting my proxy server configuration finished.  Need to clean up the redis stuff

---
+++ files/get_app_server_location.lua
@@ -0,0 +1,21 @@
+local red, err = redis:new()
+red:connect("127.0.0.1", 6379)
+
+-- ngx.print(err)
+
+local server = red:hget("servers", ngx.var.host)
+ngx.log(ngx.INFO, "connecting to server: ", server)
+
+if server == ngx.null then
+        ngx.exit(ngx.HTTP_NOT_FOUND)
+else
+        ngx.var.app = server
+end
+
+
+-- ngx.print(red:keys("*"))
+-- ngx.print(server)
+
+-- ngx.print(ngx.req.get_headers()["Host"])
+-- ngx.print(ngx.var.host)
+
